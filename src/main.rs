e464b563b10b3bb2d5154bab4f05681e7c1c4459
itsf4llofstars <irooted4hal@mailfence.com>
2023-09-05 23:59:28 -0500

    Float precision, math operators, increment

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@
 use std::io;
 // generate random values
 use rand::Rng;
+
 /** Nested importing of libraries
  * these are to be covered later in the
  * lecture
@@ -11,15 +12,29 @@ use rand::Rng;
  use std::io::{Write, BufReader, BufRead, ErrorKind};
  use std::fs::File;
  use std::cmp::Ordering;
- 
+
 
 fn main() {
-    /** use let _is_true (underscore pre-pended)
-     * to prevent rust compiler from erroring on
-     * unused variables when we are not using
-     * #![allow(unused)]
-     */
-    let is_true = true; // false
+    // floating point precision
 
-    let my_grade = 'A'; // char
+    let num_1: f32 = 1.111111111111111; // 15 ones
+    println!("f32: {}", num_1 + 0.111111111111111);
+
+    let num_2: f64 = 1.111111111111111; // 15 ones
+    println!("f64: {}", num_2 + 0.111111111111111);
+
+    // math operators
+    let num_3: u32 = 50;
+    let num_4: u32 = 4;
+    println!("50 + 4 = {}", num_3 + num_4);
+    println!("50 - 4 = {}", num_3 - num_4);
+    println!("50 * 4 = {}", num_3 * num_4);
+    println!("50 / 4 = {}", num_3 / num_4);
+    println!("50 % 4 = {}", num_3 % num_4);
+
+    // increment
+    let mut inc_num: u32 = 100;
+    println!("inc_num = {}", inc_num);
+    inc_num += 1;
+    println!("inc_num = {}", inc_num);
 }
