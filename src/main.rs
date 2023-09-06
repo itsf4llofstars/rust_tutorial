eff1786f7262b7d6833733fafa33018f907ebe69
itsf4llofstars <irooted4hal@mailfence.com>
2023-09-05 23:19:08 -0500

    Import tutorial required libraries
    First run of main: hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+#![allow(unused)]
+
+// use standard input/output library
+use std::io;
+// generate random values
+use rand::Rng;
+/** Nested importing of libraries
+ * these are to be covered later in the
+ * lecture
+ */
+ use std::io::{Write, BufReader, BufRead, ErrorKind};
+ use std::fs::File;
+ use std::cmp::Ordering;
+ 
+
 fn main() {
     println!("Hello, world!");
 }
