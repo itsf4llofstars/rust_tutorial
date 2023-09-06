04294d782bf748196dae8aabdd1689dc2eb831ba
itsf4llofstars <irooted4hal@mailfence.com>
2023-09-05 23:29:44 -0500

    User interaction example

--- src/main.rs
+++ src/main.rs
@@ -14,5 +14,11 @@ use rand::Rng;
  
 
 fn main() {
-    println!("Hello, world!");
+    println!("What is your name?");
+    let mut name = String::new(); // mutable string var
+    let greeting = "Nice to meet you"; // string var
+    io::stdin().read_line(&mut name) // get input from user
+        .expect("Didn't Receive Input");
+
+    println!("Hello {}! {}", name.trim_end(), greeting); // print result
 }
